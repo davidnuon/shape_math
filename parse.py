ac415c237cd82eae78d1162c86c17dec3bec1ae1
David Nuon <david@davidnuon.com>
2014-11-06 23:19:52 -0800

    Squares

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@
 
 import sys
 import cr
+import sq
 
 the_filename = sys.argv[1]
 
@@ -14,6 +15,25 @@ def _tonum(t):
 def filter_by_variable(list, cat):
 	return filter(lambda x : x[2] == cat, list)
 
+def process_circles(things):
+	print "Top Center: %s " % str(cr.top_center(things))
+	print "Left Center: %s" % str(cr.left_center(things))
+	print "Right Center: %s" % str(cr.right_center(things))
+	print "Bottom Center: %s" % str(cr.bottom_center(things))
+	print "Top Left: %s" % str(cr.top_left(things))
+	print "Top Right: %s" % str(cr.top_right(things))
+	print "Bottom Left: %s" % str(cr.bottom_left(things))
+	print "Bottom Right: %s" % str(cr.bottom_right(things))
+
+def process_squares(things):
+	print "Top Center: %s " % str(sq.top_center(things))
+	print "Left Center: %s" % str(sq.left_center(things))
+	print "Right Center: %s" % str(sq.right_center(things))
+	print "Bottom Center: %s" % str(sq.bottom_center(things))
+	print "Top Left: %s" % str(sq.top_left(things))
+	print "Top Right: %s" % str(sq.top_right(things))
+	print "Bottom Left: %s" % str(sq.bottom_left(things))
+	print "Bottom Right: %s" % str(sq.bottom_right(things))
 
 def main():
 	buffy = ""
@@ -23,16 +43,9 @@ def main():
 	lines = buffy.split("\r")
 	lines = map(lambda x : map(_tonum, x.replace('\n', '').split('\t')), lines)
 
-	things =  filter_by_variable(lines, 1.0)
+	things =  filter_by_variable(lines, 2.0)
+	process_squares(things)
 
-	print "Top Center: %s " % str(cr.top_center(things))
-	print "Left Center: %s" % str(cr.left_center(things))
-	print "Right Center: %s" % str(cr.right_center(things))
-	print "Bottom Center: %s" % str(cr.bottom_center(things))
-	print "Top Left: %s" % str(cr.top_left(things))
-	print "Top Right: %s" % str(cr.top_right(things))
-	print "Bottom Left: %s" % str(cr.bottom_left(things))
-	print "Bottom Right: %s" % str(cr.bottom_right(things))
 
 if __name__ == '__main__':
 	main()
--- sq.py
+++ sq.py
@@ -1,39 +1,85 @@
+from lib import *
+import cr
+
 """
-•	TopCENTER 
-	•	Filter data by selecting values above average for all Y values  
-	•	To find TopCENTER X value, add maximum and minimum X-values and then divide by 2. 
-	•	To Find TopCENTER Y-value, use VLOOKUP command with TopCENTER X value to find the corresponding y-value. 
+	TopCENTER 
+	Filter data by selecting values above average for all Y values  
+	To find TopCENTER X value, add maximum and minimum X-values and then divide by 2. 
+	To Find TopCENTER Y-value, use VLOOKUP command with TopCENTER X value to find the corresponding y-value. 
 """
 
-#	0    1    2    3   4        5            6  
-# [4.0, 'c', 1.0, 'c', 1393.0, 63.40470236, 43.4568177]
 def top_center(rows):
-	
-	# meanY = sum(map(lambda x : x[6], rows))/len(rows)
+	return cr.top_center(rows)
 
-"""
-	•	LeftCENTER 
-	•	Filters data by selecting cases below average in the X values
-	•	Subtract maximum and minimum Y value and divide by 2 to find center Y values
-	•	Use VLOOKUP command with center Y value to find center X value
-	•	RightCENTER 
-	•	Filter data by selecting cases above average  for  all X values
-	•	Subtract maximum and Minimum y value and divide by 2 to find center Y value
-	•	Use VLOOKUP command with center y axis to find center X value 
-	•	BottomCENTER 
-	•	Filter data by selecting values below average for all Y values 
-	•	Subtract maximum  from minimum  X value and then divide by 2 to find center X value 
-	•	Use VLOOKUP command with center X value to find center Y value 
-	•	TopLEFT
-	•	Filter data by selecting values below average for all X values and selecting all values greater than the average in all Y values
-	•	Find minimum x value and  find maximum y value
-	•	TopRIGHT
-	•	Filter data by selecting values above average for all  X values and selecting values above average for all  Y values
-	•	Find maximum x value and find maximum y value
-	•	BottomLEFT
-	•	Filter data by selecting values below average for all X values and selecting values below average for all Y values
-	•	Find minimum X value  and find minimum  Y value
-	•	BottomRIGHT
-	•	Filter data by selecting values above average for X values and selecting values below average for all Y values
-	•	Find maximum X values and find minimum Y value 
-""""
+def left_center(rows):
+	return cr.left_center(rows)
+
+def right_center(rows):
+	return cr.right_center(rows)
+
+def bottom_center(rows):
+	return cr.bottom_center(rows)
+
+def top_left(rows):
+	yCoords, xCoords, theCoords = parse_rows(rows)
+	meanX = mean(xCoords)
+	meanY = mean(yCoords)
+	bigList = filter(lambda v : v[X_AXIS] < meanX and v[Y_AXIS] > meanY, theCoords)
+	minX = min(map(lambda v: v[X_AXIS], bigList))
+	maxY = max(map(lambda v: v[Y_AXIS], bigList))
+	return (minX, maxY)
+
+def top_right(rows):
+	yCoords, xCoords, theCoords = parse_rows(rows)
+	meanX = mean(xCoords)
+	meanY = mean(yCoords)
+	bigList = filter(lambda v : v[X_AXIS] > meanX and v[Y_AXIS] > meanY, theCoords)
+	minX = max(map(lambda v: v[X_AXIS], bigList))
+	maxY = max(map(lambda v: v[Y_AXIS], bigList))
+	return (minX, maxY)
+
+def bottom_left(rows):
+	yCoords, xCoords, theCoords = parse_rows(rows)
+	meanX = mean(xCoords)
+	meanY = mean(yCoords)
+	bigList = filter(lambda v : v[X_AXIS] < meanX and v[Y_AXIS] < meanY, theCoords)
+	minX = min(map(lambda v: v[X_AXIS], bigList))
+	maxY = min(map(lambda v: v[Y_AXIS], bigList))
+	return (minX, maxY)
+
+def bottom_right(rows):
+	yCoords, xCoords, theCoords = parse_rows(rows)
+	meanX = mean(xCoords)
+	meanY = mean(yCoords)
+	bigList = filter(lambda v : v[X_AXIS] > meanX and v[Y_AXIS] < meanY, theCoords)
+	minX = max(map(lambda v: v[X_AXIS], bigList))
+	maxY = min(map(lambda v: v[Y_AXIS], bigList))
+	return (minX, maxY)
+
+
+# """
+# 	LeftCENTER 
+# 	Filters data by selecting cases below average in the X values
+# 	Subtract maximum and minimum Y value and divide by 2 to find center Y values
+# 	Use VLOOKUP command with center Y value to find center X value
+# 	RightCENTER 
+# 	Filter data by selecting cases above average  for  all X values
+# 	Subtract maximum and Minimum y value and divide by 2 to find center Y value
+# 	Use VLOOKUP command with center y axis to find center X value 
+# 	BottomCENTER 
+# 	Filter data by selecting values below average for all Y values 
+# 	Subtract maximum  from minimum  X value and then divide by 2 to find center X value 
+# 	Use VLOOKUP command with center X value to find center Y value 
+# 	TopLEFT
+# 	Filter data by selecting values below average for all X values and selecting all values greater than the average in all Y values
+# 	Find minimum x value and  find maximum y value
+# 	TopRIGHT
+# 	Filter data by selecting values above average for all  X values and selecting values above average for all  Y values
+# 	Find maximum x value and find maximum y value
+# 	BottomLEFT
+# 	Filter data by selecting values below average for all X values and selecting values below average for all Y values
+# 	Find minimum X value  and find minimum  Y value
+# 	BottomRIGHT
+# 	Filter data by selecting values above average for X values and selecting values below average for all Y values
+# 	Find maximum X values and find minimum Y value 
+# """"
